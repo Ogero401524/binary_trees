d2bb02d25636a5d0a4cb0642c7b0219e3bb235de
Ogero401524 <amirakraphael@gmail.com>
2024-02-29 17:27:38 -0800

    insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 
 	newNR = binary_tree_node(parent, value);
-	if (new == NULL)
+	if (newNR == NULL)
 		return (NULL);
 
 	if (parent->right != NULL)
